ad5dce687d4c9f967cda1b0df6cec04f6a2126a2
Guo Zhengjin <guo.zhengjin@gmail.com>
2019-07-04 16:50:23 +0800

    MiCard with custom font, circle avatar, rounded corners, etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,31 +10,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: AssetImage('images/profile_pic.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.lightGreen,
-                  ),
-                ],
+              Text(
+                'Angela Yu',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontFamily: 'Satisfy',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
               ),
               Container(
-                width: 100.0,
-                color: Colors.blue,
+                margin: EdgeInsets.fromLTRB(50, 15, 50, 5),
+                padding: EdgeInsets.only(left: 10),
+                // color: Colors.white,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4.0),
+                  ),
+                ),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.call,
+                      color: Colors.teal,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text('+44 123 4567 8901'),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(50, 5, 50, 5),
+                padding: EdgeInsets.only(left: 10),
+                // color: Colors.white,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4.0),
+                  ),
+                ),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text('angela@gmail.com'),
+                  ],
+                ),
               ),
             ],
           ),
